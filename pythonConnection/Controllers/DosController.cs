b0cf21f9418b9d606b7bb40902b4f22cc036defe
Ayala214 <ayalaf@darkeychana.com>
2022-12-02 10:43:47 +0200

    my commit message

---
+++ pythonConnection/Controllers/DosController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace pythonConnection.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DosController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<int> DoSomething(int x)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync($"http://127.0.0.1:5000/do?x={x}");
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        Console.Write("Success");
+                    }
+                    else
+                    {
+                        Console.Write("Failure");
+                    }
+
+                    return int.Parse(await response.Content.ReadAsStringAsync());
+                }
+            }
+            catch (Exception e)
+            {
+                return 0;
+            }
+        }
+    }
+}
